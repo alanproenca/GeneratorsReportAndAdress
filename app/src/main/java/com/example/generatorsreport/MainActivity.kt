49a635a083f07f0f05208579118b627182393f4c
Alan <alanproenca.dev@gmail.com>
2022-12-13 18:06:08 -0300

    Initial commit

---
+++ app/src/main/java/com/example/generatorsreport/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.generatorsreport
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnReport: ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnReport = findViewById(R.id.icReport)
+
+        btnReport.setOnClickListener {
+
+          val i = Intent(this, reportGeneratorActivity::class.java )
+          startActivity(i)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/generatorsreport/reportGeneratorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.generatorsreport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reportGeneratorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_generator)
+    }
+}
